0a8447f23cd636e019c9fd7968dd83ea39766d58
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2021-01-08 02:44:03 +0100

    Add error exit code to statuscharge.sh

--- statuscharge.sh
+++ statuscharge.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 BAT="/sys/class/power_supply/BAT0"
-[ -d "$BAT" ] || exit
+[ -d "$BAT" ] || exit 1
 printf "%s%d%%\n" "$(sed 's/Discharging/🔋/; s/Charging\|Full\|Unknown/🔌/' < $BAT/status)" "$(cat $BAT/capacity)"
